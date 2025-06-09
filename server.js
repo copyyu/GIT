c1807b48add0b00b2091d3b5f9f2dab500769096
copy yu <moonlightvilip@gmail.com>
2025-06-09 11:22:14 +0700

    เพิ่มฟิลด์ใหม่ใน clicklog และอัปเดต logic logging

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const { Pool } = require('pg');
 require('dotenv').config(); // เผื่อยังไม่ได้ require dotenv
 const app = express();
+const geoip = require('geoip-lite');
+const UAParser = require('ua-parser-js');
 
 const mapping = JSON.parse(fs.readFileSync('mapping.json', 'utf8'));
 const codeToEmail = {};
@@ -23,12 +25,42 @@ app.get('/file/:code', async (req, res) => {
 
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || '';
   const userAgent = req.get('User-Agent') || '';
-  const time = new Date().toISOString();
+  const referrer = req.get('Referer') || '';
+  const time_clicked = new Date().toISOString();
+
+  // GeoIP
+  const geo = geoip.lookup(ip) || {};
+  // User Agent
+  const parser = new UAParser();
+  const ua = parser.setUA(userAgent).getResult();
+
+  // Campaign name (query param หรือ default)
+  const campaign_name = req.query.campaign || 'default';
+
+  // Reaction time (null ถ้าไม่มี landing page)
+  const reaction_time = null;
 
   try {
     await pool.query(
-      'INSERT INTO clicklog(email, code, ip, useragent, time) VALUES ($1, $2, $3, $4, $5)',
-      [email, code, ip.replace(/[\r\n,]/g,''), userAgent.replace(/[\r\n,]/g,''), time]
+      `INSERT INTO clicklog(
+        email, code, ip, location_country, location_city, useragent,
+        device_type, os, browser, referrer, campaign_name, time_clicked, reaction_time
+      ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)`,
+      [
+        email,
+        code,
+        ip.replace(/\r|\n|,/g, ''),
+        geo.country || '',
+        geo.city || '',
+        userAgent.replace(/\r|\n|,/g, ''),
+        ua.device.type || '',
+        ua.os.name + ' ' + ua.os.version,
+        ua.browser.name + ' ' + ua.browser.version,
+        referrer,
+        campaign_name,
+        time_clicked,
+        reaction_time
+      ]
     );
   } catch (err) {
     console.error('Log insert error:', err);
